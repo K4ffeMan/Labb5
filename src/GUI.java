4ab4a60430090e3c194085897b069a7aad487f23
K4FFEM4N <80048836+K4ffeMan@users.noreply.github.com>
2023-02-21 14:45:00 +0100

    Lite kod

--- src/GUI.java
+++ src/GUI.java
@@ -1,8 +1,45 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+
 
 class GUI extends JFrame implements ActionListener {
+    private static JTextField inputBox;
+    private static void display(){
+        JFrame frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        createUI(frame);
+
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null); //Centrerar miniräknaren (förhoppningsvis)
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
+    private static void createUI(JFrame frame){
+        JPanel panel = new JPanel();
+        GUI calculator = new GUI();
+
+
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints gbc = new GridBagConstraints();
+        panel.setLayout(layout);
+
+        inputBox = new JTextField(10);
+        inputBox.setEditable(false);
+
+        
+
+
+
+    }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
---
+++ src/Situation.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Situation {
+    State state = State.Input1;
+    JLabel display;
+    BinOpButton binaryOperator;
+    int leftOperand;
+
+    Situation(JLabel display){
+        this.display = display;
+    }
+
+    private enum State{
+        Input1, OpReady, Input2, HasResult
+    }
+}
