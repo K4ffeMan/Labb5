c016949e660ea9b35cc3b2da4c9ba99d2ae30f96
Skoldator <cool23man23@gmail.com>
2023-02-22 10:18:42 +0100

    gjorde klart (kanske) DigitButton och EqualsButton

--- src/labb5/buttons/DigitButton.java
+++ src/labb5/buttons/DigitButton.java
@@ -2,13 +2,32 @@ package labb5.buttons;
 
 import labb5.GUI.Situation;
 
-public class DigitButton extends CalculatorButton{
+import javax.swing.*;
+
+public class DigitButton extends CalculatorButton {
     public DigitButton(String textIn, Situation situationIn) {
         super(textIn, situationIn);
     }
 
     @Override
     public void transition() {
+        Situation situation = getSituation();
+        JLabel display = situation.getDisplay();
 
+        if (situation.getState() == Situation.State.OpReady) {
+            situation.setState(Situation.State.Input2);
+            display.setText(getText());
+            return;
+        }
+
+        String current = display.getText();
+
+
+        if (current.equals("0") || situation.getState() == Situation.State.HasResult) {
+            display.setText(getText());
+            return;
+        }
+
+        display.setText(current + getText());
     }
 }
--- src/labb5/buttons/EqualsButton.java
+++ src/labb5/buttons/EqualsButton.java
@@ -1,14 +1,22 @@
 package labb5.buttons;
 
 import labb5.GUI.Situation;
+import labb5.verktyg.Konstanter;
 
 public class EqualsButton extends CalculatorButton {
-    public EqualsButton(String textIn, Situation situationIn) {
-        super(textIn, situationIn);
+    public EqualsButton(Situation situationIn) {
+        super("=", situationIn);
     }
 
     @Override
     public void transition() {
-
+        Situation situation = getSituation();
+        if (situation.getState() == Situation.State.Input2) {
+            int result = situation.getBinaryOperator().applyAsInt(
+                    situation.getLeftOperand(),
+                    Integer.parseInt(situation.getDisplay().getText()));
+            situation.getBinaryOperator().setColor(Konstanter.BUTTON_BORDER_COLOR);
+            situation.getDisplay().setText(result + "");
+        }
     }
 }
